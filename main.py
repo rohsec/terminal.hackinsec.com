de3cd9e73061c917e055d3acb3e6f60966c1ca25
rohsec <marcosiaf07@gmail.com>
2024-02-11 20:59:07 +0530

    Major Fixes

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from time import sleep
 from pyscript import document
 pyodide_http.patch_all()
 
+## remove load banner
+xx=document.getElementById('load')
+xx.remove()
 bred='\033[1;31m'
 bblue='\033[1;34m'
 bgreen='\033[1;32m'
@@ -14,9 +17,9 @@ blue='\033[0;34m'
 green='\033[0;32m'
 reset='\033[0m'
 
-commands=["help","twitter","about","extensions","coffee","joke","crt","exit"]
+commands=["help","learn","about","extensions","coffee","joke","crt","exit"]
 extensions=["AutoRepeater", "Js Link Finder", "GAP", "Piper", "Reflection", "Hackverter"]
-learn_list={"TomNomNom":"TomNomNomDotCom","NahamSec":"nahamsec"}
+learn_list={"TomNomNom":"TomNomNomDotCom","NahamSec":"nahamsec","Jason Haddix":"jhaddix","IppSec":"ippsec","John Hammond":"_JohnHammond","LiveOverflow ":"LiveOverflow","g0lden":"g0lden1"}
 headers={'Accept':'application/json'}
 BASE_URL = "https://crt.sh/?q={}&output=json"
 subdomains = set()
@@ -30,11 +33,11 @@ def banner():
 |_|  \___/|_| |_|___/\___|\___|
 {blue}<--- {yellow}https://x.com/rohsec{blue} --->{reset}
 """)
-    print(" Hello World !! Welcome to the matrix, Enter help to check the available commands")
+    print("Hello World !! Welcome to the matrix, Enter help to check the available commands")
 
 def help():
-    print(" Available Commands :")
-    print(" ====================")
+    print("Available Commands :")
+    print("====================")
     print("""  
     help -- displays available commands
     crt -- Extract Subdomains from Crt.sh
@@ -45,34 +48,34 @@ def help():
     exit -- Exit the app
 """)
 def extension():
-    print(" My current Burp Extensions are :")
+    print(f"{yellow}[{bgreen} *{yellow} ]{reset} My current Burp Extensions are :")
     for i in extensions:
         print(i)
 def learn():
-    print(" Resources to get started : ")
+    print(f"{yellow}[{bgreen} *{yellow} ]{reset} Resources to get started :\n\t1.PortSwigger Academy : https://portswigger.net/web-security\n\t2.Hacker101 : https://hacker101.com")
 
-    print(" Content Creators to check out :")
+    print(f"{yellow}[{bgreen} *{yellow} ]{reset} Content Creators to check out :")
     for key in learn_list:
-        print(f"{blue}{key} {green}=> {yellow}https://youtube.com/@{learn_list[key]}" )
+        print(f"\t{blue}{key} {green}=> {yellow}https://youtube.com/@{learn_list[key]}" )
 
 def about():
-    print(f"{red} About{reset}")
-    print(f"{blue} ================{reset}")
-    print(" Hi 👋! I am Rohit a.k.a rohsec. I am a full time BugBounty Hunter and HackerOne Ambassador. I like identifying vulnerabilities and helping organizations reinforce their defenses. I have ethically hacked and secured various big techs such as Sony, RedBull, BBC, Dutch Government etc.")
-    print(f"\n{yellow} Follow me:{reset}\n https://twitter.com/rohsec")
+    print(f"{red}About{reset}")
+    print(f"{blue}================{reset}")
+    print("Hi 👋! I am Rohit a.k.a rohsec. I am a full time BugBounty Hunter and HackerOne Ambassador. I like identifying vulnerabilities and helping organizations reinforce their defenses. I have ethically hacked and secured various big techs such as Sony, RedBull, BBC, Dutch Government etc.")
+    print(f"\n{yellow}Follow me:{reset}\n https://twitter.com/rohsec")
 
 def coffee():
     print("If you appreciate the work I do, consider supporting my work! Your small contribution can help me continue my work for the community.")
-    print(f"\n{yellow} Buy Me a Coffee{reset} 🙌{yellow} :{reset}\n https:/buymeacoffee/rohsec")
+    print(f"\n{yellow}Buy Me a Coffee{reset} 🙌{yellow} :{reset}\nhttps:/buymeacoffee/rohsec")
 
 def jokes():
-    print(f" {yellow}[ {bblue}+{yellow} ]{reset} Fetching a joke for you...")
+    print(f"{yellow}[ {bblue}+{yellow} ]{reset} Fetching a joke for you...")
     resp=req.get("https://icanhazdadjoke.com/",headers=headers)
     if(resp.ok):
         respjson=json.loads(resp.content)
-        print(f" {respjson['joke']}")
+        print(f"{respjson['joke']}")
     else:
-        print("Opps !! Techincal error occured, try the command again !")
+        print("Opps !! Techincal error occured, maybe try after some time !")
 
 def crtsh():
     BASE_URL = "https://crt.sh/?q={}&output=json"
@@ -80,7 +83,7 @@ subdomains = set()
 wildcardsubdomains = set()
 
 def crtsh():
-    domain=input(f" {blue}Enter a domain:{reset} ")
+    domain=input(f"{blue}Enter a domain:{reset} ")
     if(domain!=""):
         try:
             print(f" {yellow}[ {bgreen}+{yellow} ]{reset} Fetching Domains...")
@@ -101,16 +104,16 @@ def crtsh():
                             else:
                                 if subname_value not in wildcardsubdomains:
                                     wildcardsubdomains.add(subname_value)
-                print(" Fetched Subdomains\n============", '\n'.join(sorted(subdomains)))
-                print(" WildCard Subdomains\n============", '\n'.join(sorted(wildcardsubdomains)))
+                print("Fetched Subdomains\n============", '\n'.join(sorted(subdomains)))
+                print("WildCard Subdomains\n============", '\n'.join(sorted(wildcardsubdomains)))
         except:
-            print(" Opps!! Techincial error occured. Please run the command again")
+            print("Opps!! Techincial error occured. Please run the command again")
     else:
-        print(f" {yellow}[ {bred}!{yellow} ]{reset} Invalid input !!")
+        print(f"{yellow}[ {bred}!{yellow} ]{reset} Invalid input !!")
 
 
 def exitapp():
-    print(" Thank you for stopping by !! You will be now redirected to my twitter, connect to get in touch :)")
+    print("Thank you for stopping by !! You will be now redirected to my twitter, connect to get in touch :)")
     sleep(2)
     document.location="https://x.com/rohsec"
 
@@ -135,7 +138,7 @@ def choice():
             exitapp()
 
     else:
-        print(" Command not found !! Run the help command to get a list of available commands")
+        print("Command not found !! Run the help command to get a list of available commands")
 
 def main():
     banner()
